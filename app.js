acaba3a4cba7a2eac69494f798c4072c53272bb4
doaashafik <doaa@cyber-crafts.com>
2021-11-04 12:01:44 +0200

    build server - add apis of linkedin social logins

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-// create an express app
-const express = require("express")
-const app = express()
+const express = require("express");
+const request = require("request");
+const quesryString = require("query-string");
+const app = express();
 
-// use the express-static middleware
-app.use(express.static("public"))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+const handleRequest = (req, res, baseUrl) => {
+  request(
+    { url: `${baseUrl}?${quesryString.stringify(req.query)}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.log(req.headers)
+        return res
+          .status(500)
+          .json({ type: "error", message: "Internal server error!" });
+      }
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+      res.json(JSON.parse(body));
+    }
+  );
+};
+app.get("/accessToken", (req, res) => {
+  const url = `https://www.linkedin.com/oauth/v2/accessToken`;
+  handleRequest(req, res, url);
+});
+app.get("/profile", (req, res) => {
+  const url = `https://api.linkedin.com/v2/me`;
+  handleRequest(req, res, url);
+});
+app.get("/email", (req, res) => {
+  const url = `https://api.linkedin.com/v2/emailAddress`;
+  handleRequest(req, res, url);
+});
+const PORT = 10000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
